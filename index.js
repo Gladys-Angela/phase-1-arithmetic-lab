26508913977dc074e2b02dd3b14a1f1ee80d0ec8
Gladys <gladysangelakamau@gmail.com>
2022-06-07 04:38:23 +0300

    phArithmetic

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+let random = 0;
+random--;
+
+let mod = (num3 % num4)
+const num3 = 28
+const num4 = 6
+mod = num3 % num4
+
+const max = Math.max (15,16,17,18,19,20);
