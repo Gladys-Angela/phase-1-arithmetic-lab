b07c0c9dfa0cdd728e54580a71674dbc96feb44f
Gladys <gladysangelakamau@gmail.com>
2022-06-12 00:05:14 +0300

    Arithmetic

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const num1 = 2
 const num2 = 31
 const multiply = num1 * num2
 
-let random = 0;
-random--;
+const random = Math.floor( Math.random() * 1000 ) + 1;
 
 let mod = (num3 % num4)
 const num3 = 28
